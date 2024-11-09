0a6dfbbec96a2d358fd251b4fd8c74fa8b9aeab6
Charlotte Sacré <chsce01@gmail.com>
2024-11-09 15:10:57 +0100

    bugfix process data

--- src/data/process_data.py
+++ src/data/process_data.py
@@ -72,7 +72,6 @@ def combine_dataframes(df_movies, df_plots, df_tmdb, common_columns, cutoffyear,
     df_cmu['clean_title'] = df_cmu['title'].str.lower().str.strip()
     df_tmdb['clean_title'] = df_tmdb['title'].str.lower().str.strip()
     df_combined = pd.merge(df_cmu, df_tmdb, on=['clean_title', 'release_year'], suffixes=("_cmu", "_tmdb"), how=how_merge)
-
     for column in common_columns:
         # create a column with combined values
         df_combined[column] = df_combined[column + "_cmu"] + df_combined[column + "_tmdb"]
@@ -82,16 +81,17 @@ def combine_dataframes(df_movies, df_plots, df_tmdb, common_columns, cutoffyear,
         df_combined.drop(column + "_tmdb", axis=1, inplace=True)
 
     # clean column names
+    df_combined.drop('title_tmdb', axis=1, inplace=True)
     colnames = [str(x).replace('_tmdb', '') for x in df_combined.columns.tolist()]
     colnames = [str(x).replace('_cmu', '') for x in colnames]
     df_combined.columns = colnames
     df_combined['overview'] = df_combined['overview'].fillna(df_combined['summary'])
 
-    # we complement our dataset with movies from cutoff onwards with TMDB dataset
+    ''' # we complement our dataset with movies from cutoff onwards with TMDB dataset
     df_tmdb_post2016 = df_tmdb[df_tmdb['release_year'] >= cutoffyear].copy()
     df_tmdb_post2016.loc[:, 'summary'] = df_tmdb_post2016['overview']
 
-    df_combined = pd.concat([df_combined, df_tmdb_post2016], axis=0)
+    df_combined = pd.concat([df_combined, df_tmdb_post2016], axis=0)'''
     df_combined.drop('clean_title', axis=1, inplace=True)
 
     return df_combined
@@ -115,7 +115,13 @@ def create_cmu_tmdb_dataset(cmu_movies_path, plots_path, tmdb_path, how_merge):
     df_tmdb = clean_string_to_list(df_tmdb, TMDB_string_columns)
     common_columns = list(set(df_movies.columns.tolist()) & set(df_tmdb.columns.tolist()))
     common_columns.remove('release_year')
+    common_columns.remove('title')
     df_combined = combine_dataframes(df_movies=df_movies, df_plots=df_plots, df_tmdb=df_tmdb,
                                      common_columns=common_columns, cutoffyear=2012, how_merge=how_merge)
     df_combined = annotate_dvd_era(df_combined)
     return df_combined
+
+def create_tmdb_dataset(tmdb_path):
+    df_tmdb = pd.read_csv(tmdb_path)
+    df_tmdb = clean_string_to_list(df_tmdb, TMDB_string_columns)
+    return annotate_dvd_era(df_tmdb)
